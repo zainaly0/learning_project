2a55a101ba3bf3a6dd76043b37dafc9ee379df68
Zaid Aly <zaidk4076@gmail.com>
2026-07-11 00:23:04 +0530

    udpated code

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-include "./config/config.php";
+include( __DIR__. "/config.php"); 
 
 $create_database_sql = "CREATE DATABASE IF NOT EXISTS auth_core";
 $db_Created = mysqli_query($conn, $create_database_sql);
@@ -23,22 +23,24 @@ $create_user_sql = "CREATE TABLE if NOT EXISTS users(
 ) ";
 
 
+
+
 $create_role_sql = "CREATE TABLE IF NOT EXISTS roles(
     id bigint unsigned auto_increment primary key,
-    name varchar(255) not null default,
+    name varchar(255) not null ,
     created_at timestamp default current_timestamp,
-    updated_at timestamp default currect_timestamp on update current_timestamp
+    updated_at timestamp default current_timestamp on update current_timestamp
 )";
 
 
-$create_password_reset_sql = "CREATE TABLE IF NOT EXIST password_reset(
+$create_password_reset_sql = "CREATE TABLE IF NOT EXISTs password_reset(
         id bigint unsigned auto_increment primary key,
         user_id bigint unsigned not null,
         token varchar(255) not null,
         expires_at timestamp not null,
         used_at timestamp null default null,
         created_at timestamp default CURRENT_TIMESTAMP,
-        foreign key(user_is) reference users(id) on delete cascade,
+        foreign key(user_id) references users(id) on delete cascade,
         index(user_id),
         index(token)
 )";
@@ -56,7 +58,7 @@ $create_remember_token_sql = "CREATE TABLE IF NOT EXISTS remember_token(
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp on update current_timestamp
 
-    foreign key(user_id) reference users(id) on delete cascade,
+    foreign key(user_id) references users(id) on delete cascade,
 
     index(user_id),
     UNIQUE(token)
@@ -78,10 +80,39 @@ $create_login_history_table = "
         logout_at timestamp default current_timestamp,
 
         login_status enum('success', 'failed') default 'success',
-        foreign key (user_id) reference on users(id) on delete cascade,
+        foreign key (user_id) REFERENCES on users(id) on delete cascade,
 
         index(user_id),
         index(login_at)
     
     )
 ";
+
+
+mysqli_begin_transaction($conn);
+try{
+
+    if(!mysqli_query($conn, $create_user_sql)){
+        die(mysqli_error($conn));
+    }
+
+
+    if(!mysqli_query($conn, $create_role_sql)){
+        die(mysqli_error($conn));
+    }
+    if(!mysqli_query($conn, $create_password_reset_sql)){
+        die(mysqli_error($conn));
+    }
+    if(!mysqli_query($conn, $create_remember_token_sql)){
+        die(mysqli_error($conn));
+    }
+    if(!mysqli_query($conn, $create_login_history_table)){
+        die(mysqli_error($conn));
+    } 
+    mysqli_commit($conn);
+
+}catch(Exception $e){
+    var_dump($e->getMessage());
+    mysqli_rollback($conn);
+
+}
