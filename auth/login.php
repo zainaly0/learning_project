c0f776671ed7b5c6db2299d7bd1b9a4a29dca4d0
Zaid Aly <zaidk4076@gmail.com>
2026-07-17 00:54:14 +0530

    working on login page

--- auth/login.php
+++ auth/login.php
@@ -5,6 +5,17 @@ require_once __DIR__ . "/../helpers/functions.php";
 
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+ 
+    $errors = [];
+
+    if(empty($email)){
+        $errors['email'] = "Email is empty";
+    }
+    if(empty($password)){
+        $
+    }
 
 
 }
@@ -61,8 +72,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         .input_group{
             margin: 2px;
         }
-        label{
-            display: block;
+        label{ 
             margin-bottom: 6px;
         }
 
@@ -89,6 +99,37 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         input[type="submit"]:hover{
             background: #0056b3;
         }
+        .options{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin: 15px 0 20px;
+        }
+
+        .remember{
+            display: flex;
+            align-items: center;
+            gap: 8px;
+            cursor: pointer;
+            font-size: 14px;
+            color: #555;
+        }
+
+        .remember input[type="chekcbox"]{
+            width: 16px;
+            height: 16px;
+            margin: 0;
+            accent-color: #007bff;
+        }
+        .options a{
+            text-decoration: none;
+            color: #007bff;
+            font-size: 14px;
+        }
+
+        .options a:hover{
+    text-decoration:underline;
+}
 
 
     </style>
@@ -112,6 +153,13 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                 <span style="color:red;">
                     <?php echo ""; ?>
                 </span>
+            </div>
+            <div class="input_group options">
+                <label class="remember">
+                    <input type="checkbox" name="remember" value="1">
+                    Remember Me
+                </label>
+                <a href="forget-password.php">Forget Password? </a>
             </div>
             
 
